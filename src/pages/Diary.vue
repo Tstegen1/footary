f1a7fdedd965082b640ec4670e374557fdfcfea4
Tstegen1 <meiyeyoutai@gmail.com>
2021-03-19 07:25:16 +0900

    diary修正中

--- src/pages/Diary.vue
+++ src/pages/Diary.vue
@@ -2,36 +2,35 @@
 <template>
   <v-main>
     <v-container>
-      <v-row>
-        <v-col cols="12" md="6">
-        <v-textarea 
-          auto-grow
-          outlined
-          name="良かったところ"
-          label="良かったところ"
-          v-model="goodText"
-        ></v-textarea>          
-        </v-col>
-        <v-col cols="12" md="6">
-        <v-textarea
-          auto-grow
-          outlined
-          name="反省点"
-          label="反省点"
-          v-model="badText"
-        ></v-textarea>          
-        </v-col>
-        <v-col cols="12" md="6">
-        <v-textarea
-          auto-grow
-          outlined
-          name="振り返り"
-          label="1日の振り返り"
-          v-model="diaryText"
-        ></v-textarea>          
+      <v-row justify="center">
+        <v-col xs="12" sm="8" md="8">
+          <v-textarea 
+            auto-grow
+            outlined
+            name="良かったところ"
+            label="良かったところ"
+            v-model="goodText"
+          ></v-textarea>          
+          <v-textarea
+            auto-grow
+            outlined
+            name="反省点"
+            label="反省点"
+            v-model="badText"
+          ></v-textarea>
+          <v-textarea
+            auto-grow
+            outlined
+            name="振り返り"
+            label="1日の振り返り"
+            v-model="diaryText"
+          ></v-textarea>          
         </v-col>
       </v-row>
     </v-container>
+    <v-btn color="primary" class="btn">
+      登録する
+    </v-btn>
   </v-main>
 </template>
 
@@ -46,4 +45,3 @@ export default {
   }
 }
 </script>
-
